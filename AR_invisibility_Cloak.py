1b1863132b92f804e617248f471bb7b0865cc989
yash17525 <yashwant2451@gmail.com>
2020-07-24 23:36:11 +0530

    init

--- AR_invisibility_Cloak.py
+++ AR_invisibility_Cloak.py
@@ -12,19 +12,41 @@ Harry :  Hey !! Would you like to try my invisibility cloak ??
          Prepare to get invisible .....................
     """)
 
-
+"""
+We will replace the current frame pixels corresponding to the cloth with the background
+pixels to generate the effect of an invisibility cloak. For this we need to store the frame of a
+static background.
+"""
 cap = cv2.VideoCapture(0)
 time.sleep(3)
 background=0
-for i in range(30):
-	ret,background = cap.read()
 
+
+for i in range(30):
+	ret,background = cap.read()    
 background = np.flip(background,axis=1)
 
+"""
+cap.read() method enables us to capture latest frame(stored in variable background) with
+the camera and it also returns a Boolean (True/False stored in ret). If frame is read
+correctly, it will be True. So you can check end of the video by checking this return value.
+"""
+
+"""
+The image captured is a bit dark compared to when multiple frames are captured. Hence capturing
+multiple images of static background with a for loop results in better image.
+"""
+
 while(cap.isOpened()):
+
+	"""
+	So the idea is that we will use a red colour cloth as out invisibility cloak. We will first
+	determine the region covered by the cloth (determine pixels corresponding to red colour).
+	To detect red colour we use the HSV colour space.
+	"""
 	ret, img = cap.read()
 	
-	# Flipping the image (Can be uncommented if needed)
+	# Flipping the image 
 	img = np.flip(img,axis=1)
 	
 	# Converting image to HSV color space.
@@ -34,7 +56,7 @@ while(cap.isOpened()):
 	blurred = cv2.GaussianBlur(hsv, value,0)
 	
 	# Defining lower range for red color detection.
-	lower_red = np.array([0,120,70])
+	lower_red = np.array([0,120,70]) # hue(color component) , saturation(amount of gray color), value (brightness)
 	upper_red = np.array([10,255,255])
 	mask1 = cv2.inRange(hsv,lower_red,upper_red)
 	
@@ -48,6 +70,12 @@ while(cap.isOpened()):
 	mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, np.ones((5,5),np.uint8))
 	
 	# Replacing pixels corresponding to cloak with the background pixels.
+	"""
+	access all the pixels which have value of 255 in
+	the final mask (The pixels corresponding to the detected red colour), and we replace the pixel
+	values with the pixel values of respective coordinates in the background frame
+	"""
+
 	img[np.where(mask==255)] = background[np.where(mask==255)]
 	cv2.imshow('Display',img)
 	k = cv2.waitKey(10)
